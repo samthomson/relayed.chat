efeeb5b8443f3477cc73868ee89868957fdbdcdb
sam <samt@samt.st>
2026-02-21 13:26:38 +0700

    comments--

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -10,11 +10,9 @@ const Messages = () => {
   return (
     <div className="min-h-screen bg-background">
       <div className="container mx-auto p-4 h-screen flex flex-col">
-        {/* Header */}
         <div className="flex items-center justify-between mb-4">
           <h1 className="text-2xl font-semibold">Messages</h1>
         </div>
-
         <DMMessagingInterface className="flex-1" />
       </div>
     </div>
