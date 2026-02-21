f3abf01820af5072fc9f341c88cc1d7408cd0593
sam <samt@samt.st>
2026-02-21 18:29:38 +0700

    remove redundant ai junk

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -10,9 +10,6 @@ const Messages = () => {
   return (
     <div className="min-h-screen bg-background">
       <div className="container mx-auto p-4 h-screen flex flex-col">
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl font-semibold">Messages</h1>
-        </div>
         <DMMessagingInterface className="flex-1" />
       </div>
     </div>
